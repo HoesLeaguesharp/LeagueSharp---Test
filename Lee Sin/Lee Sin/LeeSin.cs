808770b8f50f0e42d1104c33758ee88e47554e70
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-12-05 17:08:53 +0200

    fix

--- Lee Sin/Lee Sin/LeeSin.cs
+++ Lee Sin/Lee Sin/LeeSin.cs
@@ -1340,16 +1340,16 @@ namespace Lee_Sin
 
             if (slot == null || !W.IsReady() ||
                 !(Player.ServerPosition.Distance(target.ServerPosition) > 350) || !(target.Distance(Player) < 1000) ||
-                 !wardFlashBool || !canwardflash) return;
+                 !wardFlashBool) return;
 
-            if ((Environment.TickCount - _lastqcasted <= 1000) &&
-                (col.Count <= 0 || Q2() || Environment.TickCount - _lastqcasted <= 1000 || Q.IsReady()) &&
-                (Environment.TickCount - _lastflashward >= 1500)) return;
+            //if ((Environment.TickCount - _lastqcasted <= 1000) &&
+            //    (col.Count <= 0 || Q2() || Environment.TickCount - _lastqcasted <= 1000 || Q.IsReady()) &&
+            //    (Environment.TickCount - _lastflashward >= 1500)) return;
                 
 
             if (Environment.TickCount -_lastwcasted > 1000 &&
                 ((Player.Position.Distance(target.Position) > 400) ||
-                 (minionss != null && minionss.Distance(Player) > 400 && minionss.HasBuff("blindmonkqtwo"))))
+                 (minionss != null)))
             {
                 WardJump(wardtotargetpos, false, false);
                 

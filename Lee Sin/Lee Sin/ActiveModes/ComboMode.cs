cc6be595b588265e1c9cfb44266db1e47fa65b93
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-12-08 10:31:01 +0200

    update

--- Lee Sin/Lee Sin/ActiveModes/ComboMode.cs
+++ Lee Sin/Lee Sin/ActiveModes/ComboMode.cs
@@ -180,9 +180,9 @@ namespace Lee_Sin.ActiveModes
             {
                 if (target.HasBuff("blindmonkqtwo") &&
                     Q.GetDamage(target) > target.Health) return;
-
-                if (Player.Health < Player.GetAutoAttackDamage(target) + 30) return;
-
+                Game.PrintChat("firstcheck");
+                if (target.Health < Player.GetAutoAttackDamage(target) + 30) return;
+                Game.PrintChat("secondCheck");
                 if (Q.IsReady() &&
                     target.Health <= R.GetDamage(target) + GetQDamage(target) + Player.GetAutoAttackDamage(target) &&
                     Q.IsReady() && target.Health > GetQDamage(target))
